d6513c55e2159966ab4bbd987958a5a8b1139bdc
ejiwen <skibnewrim@gmail.com>
2021-07-26 13:43:56 -0700

    get the list of product back from API

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/products/', {
+    headers: {
+      Authorization: `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`
+    }
+    })
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  });
+
 
   return (
     <div>
