2c15913eefb529e7fa3b985ad9ce4be45aff8a46
Cheikhany ould Ejiwen <skibnewrim@gmail.com>
2021-11-02 10:15:10 -0700

    Update GaleryComponent.js

--- client/src/components/GaleryComponent.js
+++ client/src/components/GaleryComponent.js
@@ -1,39 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import $ from 'jquery';
-import axios from 'axios';
-import lazy from '../scripts/lazyLoad.js';
-import picSelected from '../scripts/picSelected.js';
-import ImageModal from '../scripts/ImageModal.js';
-import Zoom from '../scripts/zoomImage.js';
-import { Thumbnails } from './styles/Container.style';
-import { PortalImg, ImgZoom } from '../components/styles/Container.style';
-import { BsFullscreen } from 'react-icons/bs';
-import { ImArrowLeft, ImArrowRight } from 'react-icons/im';
-import styles from '../data/styles.js';
+import React, { useEffect, useState, useCallback } from "react";
+import $ from "jquery";
+import lazy from "../scripts/lazyLoad.js";
+import picSelected from "../scripts/picSelected.js";
+import ImageModal from "../scripts/ImageModal.js";
+import Zoom from "../scripts/zoomImage.js";
+import { Thumbnails } from "./styles/Container.style";
+import { PortalImg, ImgZoom } from "../components/styles/Container.style";
+import { BsFullscreen } from "react-icons/bs";
+import { ImArrowLeft, ImArrowRight } from "react-icons/im";
+import styles from "../data/styles.js";
 
 const GaleryComponent = ({ productID, styleIndex, widenFn }) => {
   const [styleProduct, setStyleProduct] = useState([]);
-  const [picIndex, setPicIndex] = useState(1);
+  const [picIndex, setPicIndex] = useState();
   const [isOpen, setIsOpen] = useState(false);
 
+  const getData = useCallback(() => {
+    let i = 0;
+    if (picIndex == undefined) {
+      setPicIndex(1);
+    }
+    setStyleProduct([]);
+    styles.results[styleIndex].photos.map((item) =>
+      setStyleProduct((prevState) => [
+        ...prevState,
+        { id: i++, thumbnail: item.thumbnail_url, url: item.url },
+      ])
+    );
+
+    lazy.leazyImg();
+  }, [styleIndex, picIndex]);
+
   useEffect(() => {
-    axios
-      .get(`/products/${productID}/styles`)
-      .then((res) => {
-        let i = 0;
-        setStyleProduct([]);
-        res.data.results[styleIndex].photos.map((item) =>
-          //item.url = item.url.replace(/&w=\d+/, "&w=10");
-          setStyleProduct((prevState) => [
-            ...prevState,
-            { id: i++, thumbnail: item.thumbnail_url, url: item.url },
-          ])
-        );
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, [styleIndex]);
+    getData();
+  }, [getData]);
 
   const carrousel = (dir) => {
     if (dir < 0) {
@@ -47,37 +47,37 @@ const GaleryComponent = ({ productID, styleIndex, widenFn }) => {
     }
 
     if (picIndex + dir <= 0) {
-      $('.previousPic').hide();
+      $(".previousPic").hide();
     } else {
-      $('.previousPic').show();
+      $(".previousPic").show();
     }
 
     if (picIndex + dir >= styleProduct.length - 1) {
-      $('.nextPic').hide();
+      $(".nextPic").hide();
     } else {
-      $('.nextPic').show();
+      $(".nextPic").show();
     }
   };
 
   const scrollFn = () => {
-    $('.thumbnails').animate({ scrollTop: +400 }, 1000);
+    $(".thumbnails").animate({ scrollTop: +400 }, 0);
   };
 
   const scrollFnTop = () => {
-    $('.thumbnails').animate({ scrollTop: -400 }, 1000);
+    $(".thumbnails").animate({ scrollTop: -400 }, 0);
   };
 
   return (
-    <div id="GalleyContainer" style={{ position: 'relative' }}>
+    <div id="GalleyContainer" style={{ position: "relative" }}>
       <span
         id="fullScreenIcon"
         style={{
-          position: 'absolute',
-          top: '20px',
-          right: '20px',
-          color: 'white',
-          fontSize: '2rem',
-          cursor: 'pointer',
+          position: "absolute",
+          top: "20px",
+          right: "20px",
+          color: "white",
+          fontSize: "2rem",
+          cursor: "pointer",
         }}
         onClick={widenFn}
       >
@@ -86,41 +86,41 @@ const GaleryComponent = ({ productID, styleIndex, widenFn }) => {
 
       <span
         style={{
-          position: 'absolute',
-          top: '45%',
-          left: '80px',
-          fontSize: 'xx-large',
-          textShadow: '3px 1px 0px #ffffffed, 5px 0px 0px rgb(0 0 0 / 15%)',
-          cursor: 'pointer',
+          position: "absolute",
+          top: "45%",
+          left: "80px",
+          fontSize: "xx-large",
+          textShadow: "3px 1px 0px #ffffffed, 5px 0px 0px rgb(0 0 0 / 15%)",
+          cursor: "pointer",
         }}
         className="previousPic"
         onClick={() => carrousel(-1)}
       >
-        <ImArrowLeft style={{ color: 'white' }} />
+        <ImArrowLeft style={{ color: "white" }} />
       </span>
       <span
         style={{
-          position: 'absolute',
-          top: '45%',
-          right: '20px',
-          fontSize: 'xx-large',
-          textShadow: '3px 1px 0px #ffffffed, 5px 0px 0px rgb(0 0 0 / 15%)',
-          cursor: 'pointer',
+          position: "absolute",
+          top: "45%",
+          right: "20px",
+          fontSize: "xx-large",
+          textShadow: "3px 1px 0px #ffffffed, 5px 0px 0px rgb(0 0 0 / 15%)",
+          cursor: "pointer",
         }}
         className="nextPic"
         onClick={() => carrousel(1)}
       >
-        <ImArrowRight style={{ color: 'white' }} />
+        <ImArrowRight style={{ color: "white" }} />
       </span>
       <Thumbnails className="thumbnails">
         {styleProduct.map((item, index) => (
           <img
             key={index}
             style={{
-              display: 'block',
-              marginBottom: '15px',
-              marginLeft: '20px',
-              border: '2px solid white',
+              display: "block",
+              marginBottom: "15px",
+              marginLeft: "20px",
+              border: "2px solid white",
             }}
             width="40px"
             height="40px"
@@ -136,13 +136,13 @@ const GaleryComponent = ({ productID, styleIndex, widenFn }) => {
 
       <span
         style={{
-          position: 'absolute',
-          top: '5px',
-          left: '35px',
-          cursor: 'pointer',
-          color: 'white',
+          position: "absolute",
+          top: "5px",
+          left: "35px",
+          cursor: "pointer",
+          color: "white",
           textShadow:
-            'rgba(0,0,0, 0.4) 0px 5px, rgba(0,0,0, 0.3) 0px 10px, rgba(0,0,0, 0.2) 0px 15px, rgba(0,0,0, 0.1) 0px 20px, rgba(0,0,0, 0.05) 0px 25px',
+            "rgba(0,0,0, 0.4) 0px 5px, rgba(0,0,0, 0.3) 0px 10px, rgba(0,0,0, 0.2) 0px 15px, rgba(0,0,0, 0.1) 0px 20px, rgba(0,0,0, 0.05) 0px 25px",
         }}
         onClick={scrollFnTop}
         className="scrollClassTop"
@@ -152,13 +152,13 @@ const GaleryComponent = ({ productID, styleIndex, widenFn }) => {
 
       <span
         style={{
-          position: 'absolute',
-          top: '318px',
-          left: '35px',
-          cursor: 'pointer',
-          color: 'white',
+          position: "absolute",
+          top: "318px",
+          left: "35px",
+          cursor: "pointer",
+          color: "white",
           textShadow:
-            'rgb(0 0 0 / 40%) 0px -5px, rgb(0 0 0 / 30%) 0px -10px, rgb(0 0 0 / 20%) 0px -15px, rgb(0 0 0 / 10%) 0px -20px, rgb(0 0 0 / 5%) 0px -25px',
+            "rgb(0 0 0 / 40%) 0px -5px, rgb(0 0 0 / 30%) 0px -10px, rgb(0 0 0 / 20%) 0px -15px, rgb(0 0 0 / 10%) 0px -20px, rgb(0 0 0 / 5%) 0px -25px",
         }}
         onClick={scrollFn}
         className="scrollClass"
@@ -166,13 +166,13 @@ const GaleryComponent = ({ productID, styleIndex, widenFn }) => {
       >
         ▼
       </span>
-      <div className="imgGalery" style={{ display: 'flex', height: '80vh' }}>
+      <div className="imgGalery" style={{ display: "flex", height: "80vh" }}>
         {styleProduct[picIndex] && (
           <img
             className="imgWiden"
             width="100%"
             height="600px"
-            src={styleProduct[picIndex].url.replace(/&w=\d+/, '&w=1000')}
+            src={styleProduct[picIndex].url.replace(/&w=\d+/, "&w=10")}
             onClick={() => setIsOpen(!isOpen)}
           />
         )}
